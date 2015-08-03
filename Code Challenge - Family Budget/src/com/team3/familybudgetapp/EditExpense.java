81211a6c5800ff69d9edb8d5b94b74c30dc0fb4e
Kevin Phair <kevin.phair@gmail.com>
2015-08-03 16:31:08 +0100

    Added input validation and JDialogs prompt related warnings and errors

--- Code Challenge - Family Budget/src/com/team3/familybudgetapp/EditExpense.java
+++ Code Challenge - Family Budget/src/com/team3/familybudgetapp/EditExpense.java
@@ -5,33 +5,44 @@ import java.awt.Component;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
-import javax.swing.text.DefaultCaret;
 
 import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Dialog.ModalExclusionType;
+import java.awt.Dialog.ModalityType;
 
-public class EditExpense {
-	private JTextField textFieldFirstName;
-	private JTextField textFieldLastName;
-	
+public class EditExpense extends JDialog {
+	private JTextField textFieldDescription;
+	private JTextField textFieldAmount;
+	private final JButton btnCancel;
+	private final JButton btnOK;
+	private final JLabel lblDescription;
+	private JLabel lblAmount;
+	private final JPanel panelEditExpense;
+	private final JDialog editExpense;
+
 	/**
-	 * TODO Merge addExpense with this
 	 * TODO Change references to ExpensesWindow to parentWindow which will be passed in 
+	 * 
 	 */
-	public EditExpense(final ArrayList<Expense> expenses){
+	public EditExpense(final Expense expense){
 		JList list = ExpensesWindow.list;
-		final Expense expense = expenses.get(list.getSelectedIndex());
-		final JFrame editExpense = new JFrame("Edit Expense");
+		editExpense = new JDialog (ExpensesWindow.editExpenses, "Add Expense");
+		editExpense.setModalityType(ModalityType.DOCUMENT_MODAL);
 		editExpense.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		try 
 		{
@@ -39,52 +50,83 @@ public class EditExpense {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		JPanel panelEditExpense = new JPanel();
+		panelEditExpense = new JPanel();
 		panelEditExpense.setLayout(null);
 		panelEditExpense.setOpaque(true);
 		editExpense.getContentPane().add(BorderLayout.CENTER, panelEditExpense);
+
+		// Add Escape and Enter key listeners so the window can be confirmed or
+		// cancelled with the keyboard
+		ActionListener escListener = new ActionListener () {
+			@Override
+			public void actionPerformed (ActionEvent e) { btnCancel.doClick (); }
+		};
+		editExpense.getRootPane ().registerKeyboardAction (escListener, KeyStroke.getKeyStroke (KeyEvent.VK_ESCAPE, 0),
+				JComponent.WHEN_IN_FOCUSED_WINDOW);
+
+		ActionListener enterListener = new ActionListener () {
+			@Override
+			public void actionPerformed (ActionEvent e) { btnOK.doClick (); }
+		};
+		editExpense.getRootPane ().registerKeyboardAction (enterListener, KeyStroke.getKeyStroke (KeyEvent.VK_ENTER, 0),
+				JComponent.WHEN_IN_FOCUSED_WINDOW);
 		
-		JLabel lblDescription = new JLabel("Description");
+		lblDescription = new JLabel("Description");
 		lblDescription.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblDescription.setBounds(10, 14, 65, 14);
 		panelEditExpense.add(lblDescription);
+		textFieldDescription = new JTextField();
+		textFieldDescription.setBounds(85, 11, 243, 20);
+		textFieldDescription.grabFocus();
+		panelEditExpense.add(textFieldDescription);
 		
-		textFieldFirstName = new JTextField();
-		textFieldFirstName.setBounds(85, 11, 243, 20);
-		panelEditExpense.add(textFieldFirstName);
-		textFieldFirstName.setColumns(10);
-		textFieldFirstName.grabFocus();
-		
-		JLabel lblAmount = new JLabel("Amount");
+		lblAmount = new JLabel("Amount");
 		lblAmount.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblAmount.setBounds(10, 45, 67, 14);
 		panelEditExpense.add(lblAmount);
-		
-		textFieldLastName = new JTextField();
-		textFieldLastName.setBounds(85, 42, 105, 20);
-		panelEditExpense.add(textFieldLastName);
-		textFieldLastName.setColumns(10);
-		
-		JButton btnOkay = new JButton("Okay");
-		btnOkay.setBounds(10, 70, 89, 23);
-		btnOkay.addActionListener(new ActionListener() {
+		textFieldAmount = new JTextField();
+		textFieldAmount.setBounds(85, 42, 105, 20);
+		panelEditExpense.add(textFieldAmount);
+
+		if (expense.description != null && expense.description.length () > 0) {
+			textFieldDescription.setText (expense.description);
+			textFieldAmount.setText (String.format("%.2f", expense.amount));
+		}
+
+		btnOK = new JButton("OK");
+		btnOK.setBounds(10, 70, 89, 23);
+		btnOK.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// Create a new Person object and add it to the ArrayList
-				if (textFieldLastName.getText().length() > 0) {
-					expense.amount = Double.parseDouble(textFieldLastName.getText());
+				// Check the input fields before changing anything in the expense object
+				if (textFieldDescription.getText ().length () == 0) {
+					JOptionPane.showMessageDialog (editExpense, 
+						"An expense description cannot be blank\n"
+						+ "You need to enter something.", 
+						"Expense description error", 
+						JOptionPane.NO_OPTION);
 				} else {
-					expense.amount = 0.0;
+					double tempAmount;
+					if (textFieldAmount.getText().length() > 0) {
+						tempAmount = Double.parseDouble(textFieldAmount.getText());
+					} else {
+						tempAmount = 0.0;
+					}
+					if (tempAmount == 0.0) {
+						if (JOptionPane.showConfirmDialog (editExpense, 
+							"There is no valid expense amount\n"
+							+ "Was this intentional?",
+							"Expense amount error", 
+							JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) != 0) return;
+					}
+					expense.amount = tempAmount;
+					expense.description = textFieldDescription.getText();
+					editExpense.dispatchEvent(new WindowEvent(editExpense, WindowEvent.WINDOW_CLOSING));
 				}
-				
-				// Update the list in the main window
-				ExpensesWindow.updateList(expenses);
-				ExpensesWindow.updateButtons(expenses);;
-				editExpense.dispatchEvent(new WindowEvent(editExpense, WindowEvent.WINDOW_CLOSING));
 			}
 		});
-		panelEditExpense.add(btnOkay);
+		panelEditExpense.add(btnOK);
 		
-		JButton btnCancel = new JButton("Cancel");
+		btnCancel = new JButton("Cancel");
 		btnCancel.setBounds(239, 70, 89, 23);
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -92,12 +134,12 @@ public class EditExpense {
 			}
 		});
 		panelEditExpense.add(btnCancel);
-		panelEditExpense.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{textFieldFirstName, textFieldLastName, btnOkay, btnCancel, lblDescription, lblAmount}));
+		panelEditExpense.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{textFieldDescription, textFieldAmount, btnOK, btnCancel, lblDescription, lblAmount}));
 		editExpense.setResizable(false);
 		editExpense.setSize(346, 128);
 		Rectangle screenSize = editExpense.getGraphicsConfiguration().getBounds();
 		editExpense.setLocation(screenSize.width / 2 - editExpense.getWidth() / 2, screenSize.height / 2 - editExpense.getHeight() / 2);
-		editExpense.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{textFieldFirstName, textFieldLastName, btnOkay, btnCancel}));
+		editExpense.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{textFieldDescription, textFieldAmount, btnOK, btnCancel}));
 		editExpense.setVisible(true);
 		
 	}
